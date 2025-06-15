85445c74d41a643a501678b96e41a81c58f67118
Kaltroniz <vivekck7404@gmail.com>
2025-06-16 01:55:54 +0530

    added reviews and error handling

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const Campgr=require('./models/campground');
 const mongoose = require('mongoose');
 const methodoverride= require('method-override');
 const campground = require('./models/campground');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
+const Joi = require('joi');
+const { title } = require('process');
+const Review = require('./models/review');
+const { reviewSchema } = require('./schemas');
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
 const db=mongoose.connection;
@@ -25,42 +31,103 @@ app.get('/',(req,res)=>{
     res.render('home')
 })
 
-app.get('/campgrounds',async (req,res)=>{
+app.get('/campgrounds',catchAsync(async (req,res)=>{
     const cg=await Campgr.find({});
     res.render('campgrounds/index',{campgrounds:cg});
-})
+}))
 app.get('/campgrounds/new',(req,res)=>{
     
     res.render('campgrounds/new');
 })
-app.post('/campgrounds',async (req,res)=>
-{
+const validation=(req,res,next)=>{
+ const schema=Joi.object({
+        campground: Joi.object({
+            title:Joi.string().required(),
+            price:Joi.number().required().min(0),
+            location:Joi.string().required(),
+            image: Joi.string().required(),
+            description:Joi.string().required()
+        }).required()
+    })
+    const {error}=schema.validate(req.body);
+    if(error){
+        const msg=error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400);
+    }
+    else{
+        next();
+    }
+}
+const reviewvalidation=(req,res,next)=>{
+    const {error}=reviewSchema.validate(req.body);
+    if(error){
+        const msg=error.details.map(el=>el.message).join(',')
+        throw new ExpressError(msg,400);
+    }
+    else{
+        next();
+    }
+}
+app.post('/campgrounds',validation,catchAsync( async (req,res)=>
+{   
+    
     const c=new Campgr(req.body.campground);
     console.log(c);
     await c.save();
     res.redirect(`/campgrounds/${c._id}`)
-})
+}))
 
-app.get('/campgrounds/:id',async (req,res)=>{
-    const cg=await Campgr.findById(req.params.id);
+app.get('/campgrounds/:id',catchAsync(async (req,res)=>{
+    const cg=await Campgr.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show',{cg});
-})
+}))
 
-app.get('/campgrounds/:id/edit',async (req,res)=>{
+app.post('/campgrounds/:id/reviews',reviewvalidation,catchAsync(async (req,res)=>{
+    const c=await Campgr.findById(req.params.id);
+    const rev=new Review(req.body.review);
+    c.reviews.push(rev);
+    await rev.save();
+    await c.save();
+    res.redirect(`/campgrounds/${c._id}`);
+}))
+
+app.get('/campgrounds/:id/edit',catchAsync(async (req,res)=>{
     const cg=await Campgr.findById(req.params.id);
     res.render('campgrounds/edit',{cg});
-})
+}))
 
-app.put('/campgrounds/:id', async (req,res)=>{
+app.put('/campgrounds/:id', validation,catchAsync(async (req,res)=>{
     const {id}=req.params;
    const c= await Campgr.findByIdAndUpdate(id,{...req.body.campground});
     res.redirect(`/campgrounds/${c._id}`)
-})
+}))
 
-app.get('/makecampground',async (req,res)=>{
+app.delete('/campgrounds/:id',catchAsync(async (req,res)=>{
+    const {id}= req.params;
+    await Campgr.findByIdAndDelete(id);
+    res.redirect('/campgrounds')
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewid', catchAsync(async (req,res)=>{
+    const {id,reviewid}=req.params;
+    await Campgr.findByIdAndUpdate(id,{$pull:{reviews:reviewid}});
+    await Review.findByIdAndDelete(reviewid);
+    res.redirect(`/campgrounds/${id}`);
+}))
+
+app.get('/makecampground',catchAsync(async (req,res)=>{
     const camp=new Campgr({title:'My garden'});
     await camp.save();
     res.send(camp);
+}))
+
+app.all(/(.*)/,(req,res,next)=>{
+    next(new ExpressError('Page Not Found', 404));
+})
+
+app.use((err,req,res,next)=>{
+ const {statusCode=500,message='something went wrong'}=err;
+ res.status(statusCode).render('error',{message,statusCode});
 })
 
 app.listen(3000,()=>{
